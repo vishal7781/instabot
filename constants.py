b8f52db05c70092edede53557870fb277f0c3b51
vishal7781 <vishalt325@gmail.com>
2017-07-11 20:52:50 +0530

    unwanted files deleted

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
 #global variable files
-APP_ACCESS_TOKEN='2869809078.ca0ed54.062cbbcabc7d4579a4781bbf8dfaa591'
+APP_ACCESS_TOKEN='2095654684.50d8178.5205725b685d40aaa6533c93843af3d7'
 #instagram api base
 BASE_URL='https://api.instagram.com/v1/'
